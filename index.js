89398ba929faca837754ce86641063247c703f8e
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-06-06 18:51:45 -0700

    adding mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-//import express from "express";
+const mongoose = require("mongoose");
 const express = require("express");
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node API Server");
-});
+mongoose
+  .connect(
+    "mongodb+srv://torres2235:!Halo2235@backenddb.puxawdm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
