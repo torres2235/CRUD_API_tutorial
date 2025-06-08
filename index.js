f6d9ca536ef561ce6b3a94c7855ebfd4c78f13b9
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-06-07 17:35:59 -0700

    update api

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express();
 
 app.use(express.json());
 
+// generic get
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
+// get all
 app.get("/api/products", async (req, res) => {
-  // get all
   try {
     const products = await Product.find({});
     res.status(200).json(products);
@@ -20,8 +21,8 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
+// get one
 app.get("/api/product/:id", async (req, res) => {
-  // get one
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
@@ -31,13 +32,31 @@ app.get("/api/product/:id", async (req, res) => {
   }
 });
 
+// post new prodcut
 app.post("/api/products", async (req, res) => {
-  // post new prodcut
   //   console.log(req.body);
   //   res.send(req.body);
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// update a product
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res.status(200).json(updatedProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
