31e113cbf890b234428c14571a46811e62b6f8c5
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-06-07 17:43:31 -0700

    delete API

--- index.js
+++ index.js
@@ -62,6 +62,22 @@ app.put("/api/product/:id", async (req, res) => {
   }
 });
 
+// delete a product
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 mongoose
   .connect(
     "mongodb+srv://torres2235:!Halo2235@backenddb.puxawdm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
